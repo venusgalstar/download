44057bb1d1f3d17d5fde84d29a27461bcf8bce43
venusgalstar <venusgalstar@gmail.com>
2022-09-04 22:34:07 +0200

    remove root folder of zip file if it contains one folder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use error_chain::error_chain;
 use std::path::Path;
 use std::fs::File;
+use std::fs::metadata;
+use std::{io, fs};
 use std::io::prelude::*;
 use std::path::PathBuf;
 use zip_extensions::*;
@@ -10,6 +12,20 @@ error_chain! {
          Io(std::io::Error);
          HttpRequest(reqwest::Error);
      }
+}
+
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
 }
 
 async fn download_and_unzip(url:&str, target:&str)-> Result<()>{
@@ -28,18 +44,36 @@ async fn download_and_unzip(url:&str, target:&str)-> Result<()>{
 
     let mut zip_file1 = File::open("./download1.zip")?;
 
-
     let mut zip = zip::ZipArchive::new(zip_file1).unwrap();
 
     zip_extensions::read::zip_extract(
         &PathBuf::from("./download1.zip"),
         &PathBuf::from(target),
     );
-    Ok(())
 
+    let count = fs::read_dir(target).unwrap().count();
+
+    if count == 1 {
+        for file in fs::read_dir(target).unwrap() {
+            let path = file.as_ref().unwrap().path();
+            let path1 = file.as_ref().unwrap().path();
+            let path2 = file.as_ref().unwrap().path();
+
+            if fs::metadata(file.as_ref().unwrap().path()).unwrap().is_dir() {
+                copy_dir_all(file.as_ref().unwrap().path(), target);
+                fs::remove_dir_all(file.as_ref().unwrap().path()).unwrap();
+            }
+        }
+    }
+    Ok(())
 }
+
+
+
 #[tokio::main]
 async fn main() -> Result<()> {
     download_and_unzip("https://github.com/minetest/minetest/releases/download/5.6.0/minetest-5.6.0-win64.zip", "versions/5.6.0").await?;
+    
+    
     Ok(())
 }
